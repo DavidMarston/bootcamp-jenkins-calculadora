4c4f125fd201cb799eb4a9a7383a0a67ea4f3f45
DavidMarston <dmaciasrdz@gmail.com>
2024-10-23 17:27:06 -0600

    Update CaluladoraImpl.java
    se implementan mas funciones de calculadora

--- src/main/java/calculadora/CaluladoraImpl.java
+++ src/main/java/calculadora/CaluladoraImpl.java
@@ -4,7 +4,28 @@ public class CaluladoraImpl implements Calculadora {
 	
 	@Override
 	public int calcular(int i1, char op, int i2) {
-		return i1+i2;
+		int res = 0;
+		switch(op){
+			case "+":
+				res= i1+i2;
+				break;
+			case "-":
+				res= i1-i2;
+				break;
+			case "x":
+				res= i1*i2;
+				break;
+			case "*":
+				res= i1*i2;
+				break;
+			case "/":
+				res= i1/i2;
+				break;
+			case "%":
+				res= i1%i2;
+				break;
+		}
+		return res;
 	}
 
 }
